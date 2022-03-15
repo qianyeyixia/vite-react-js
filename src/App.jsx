bee67e8dbb4fcd1b39833220d200b0da701a4100
wangyi <2851813754@qq.com>
2022-03-15 13:39:25 +0800

    config🔧:  同步配置

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,16 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-import { Button } from 'antd'
+import React from "react"
+import {HashRouter as Router, Route } from "react-router-dom"
 
 const App = () => {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <Button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </Button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React123
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <Router>
+      <Route
+        path="/"
+        key="container"
+        render={(props) => <Container {...props} />}
+      />
+  </Router>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {Provider as ReduxProvider} from "react-redux"
+import {store, persistor} from "@/store"
+import {ConfigProvider} from "antd"
+import zhCN from 'antd/es/locale/zh_CN'
+import moment from 'moment'
+import 'moment/locale/zh-cn'
+moment.locale('zh-cn')
+
 import './index.css'
 import App from './App'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ReduxProvider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <ConfigProvider locale={zhCN}>
+        <App />
+      </ConfigProvider>
+    </PersistGate>
+  </ReduxProvider>,
   document.getElementById('root')
 )
