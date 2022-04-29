1e896bdcdc9df994e514aa96c45b1e4198bef7d5
wangyi <2851813754@qq.com>
2022-04-29 17:34:45 +0800

    config🔧:  增加ts，eslint,prrettier 配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,38 @@
-module.exports = {
+export default {
   extends: [
     "eslint:recommended",
-    "prettier",
+    "plugin:react/recommended",
+    "plugin:import/recommended",
+    "plugin:prettier/recommended",
   ],
-  plugins: ["jsx-a11y", "import", "react", "react-hooks"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "react", "jsx-a11y", "import", "react-hooks", "prettier"],
   rules: {
     "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn"
+    "react-hooks/exhaustive-deps": "warn",
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "no-unused-vars": [
+      "error",
+      {
+        vars: "all",
+        args: "after-used",
+        ignoreRestSiblings: false,
+      },
+    ]
   },
   env: {
-    "browser": true,
-    "node": true,
-    "es6": true
+    browser: true,
+    node: true,
+    es6: true,
   },
   parserOptions: {
-    "ecmaVersion": 2021,
-    "sourceType": "module",
-    "ecmaFeatures": {
-      "jsx": true
-    }
-  }
-}
+    ecmaVersion: 2021,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+      experimentalObjectRestSpread: true,
+      // 修饰器
+      experimentalDecorators: true,
+    },
+  },
+};
