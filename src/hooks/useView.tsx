f56e7c6bacdb8b425b1bed51d496ff6c8819c08e
wangyi <2851813754@qq.com>
2022-05-11 10:29:27 +0800

    perf👌:  使用ts重写hooks 方法

--- src/hooks/useView.tsx
+++ src/hooks/useView.tsx
@@ -1,11 +1,28 @@
 
 import { createContext, useContext } from 'react'
 
-const ViewContext = createContext({})
+import { Action } from '@/layout/tagsView/common'
+
+import type {Dispatch, ReactNode} from "react"
+
+
+interface ViewContext {
+  name?:string
+  dispatch?: Dispatch<Action>
+  mate?:any
+}
+
+
+const ViewContext = createContext<ViewContext>({})
 const Provider = ViewContext.Provider
 export const useView = () => {
 	// const routeContext = React.useContext(RouteContext)
 	return useContext(ViewContext)
 }
 
-export const ViewProvider = ({ value, children }) => <Provider value={value}>{children}</Provider>
+interface Props {
+  children: ReactNode | JSX.Element
+  value: ViewContext
+}
+
+export const ViewProvider = ({ value, children }: Props) => <Provider value={value}>{children}</Provider>
