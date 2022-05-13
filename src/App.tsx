991af6bcacb1a1f570ee5d1f7cbffd01cba0db82
wangyi <2851813754@qq.com>
2022-05-13 17:10:52 +0800

    refactor🎨:  全局入口文件更新

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,21 @@
 import React, { StrictMode, FC } from "react";
-import { BrowserRouter } from "react-router-dom";
 
-import { ConfigProvider } from "antd";
 import { Provider as ReduxProvider } from "react-redux";
 import { PersistGate } from "redux-persist/lib/integration/react";
 import BackLayout from "@/layout";
 
-import zhCN from "antd/es/locale/zh_CN";
 
 import { store, persistor } from "@/store";
 import '@/assets/css/public.less';
 
-
+import { SyncRouter } from '@/router'
 
 const App:FC = () => {
   return (
     <StrictMode>
       <ReduxProvider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <ConfigProvider locale={zhCN}>
-            <BrowserRouter>
-              <BackLayout />
-            </BrowserRouter>
-          </ConfigProvider>
+          <SyncRouter/>
         </PersistGate>
         </ReduxProvider>
     </StrictMode>
