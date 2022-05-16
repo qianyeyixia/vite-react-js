fed3009ee61e453c3c541e2db68a7e1e1ff599a4
wangyi <2851813754@qq.com>
2022-05-16 15:33:54 +0800

    refactor🎨:  增加hooks 测试分页切换组件状态是否小时

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,16 @@
+import { Button } from 'antd';
 import React from 'react';
 
 function Home() {
+  const [count, setCount] = React.useState(0);
   return (
     <div>
       Home
+      <Button onClick={() => {
+        setCount(count + 1);
+      }}>
+        {count} ++
+      </Button>
     </div>
   );
 }
--- src/pages/list/details.jsx
+++ src/pages/list/details.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import { Button } from 'antd';
 function ListDetails() {
+  const [count, setCount] = React.useState(5);
   return (
     <div>
       ListDetails
+      <Button onClick={() => {
+        setCount(count + 1);
+      }}>
+        {count} ++
+      </Button>
     </div>
   );
 }
