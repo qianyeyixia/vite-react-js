0b76021496c4d94ef8ff221c104897df2cbefa40
wangyi <2851813754@qq.com>
2022-05-16 15:35:21 +0800

    file📦:  全局store 下 app 模块参数调整

--- src/store/slicers/appSlice.ts
+++ src/store/slicers/appSlice.ts
@@ -1,18 +1,20 @@
 import { createSlice } from '@reduxjs/toolkit'
 import type { RootState } from '@/store'
-
+import {TagsViewDto} from "@/layout/tagsView/common";
 export interface AppState {
   theme: "light" | "dark"
   collapsed: boolean // 菜单收纳状态, 用于垂直布局
   menuMode: 'horizontal' | 'vertical' // 菜单模式, 用于水平布局
-  width: number
+  width: number,
+  keepAliveList: TagsViewDto[]
 }
 
 const initialState:AppState = {
   collapsed: false,
   theme: 'dark',
   menuMode: 'horizontal',
-  width: 200
+  width: 200,
+  keepAliveList: []
 }
 
 export const appSlice = createSlice({
@@ -31,14 +33,18 @@ export const appSlice = createSlice({
     },
     setWidth(state, action) {
       state.width = action.payload
+    },
+    setKeepAliveList(state, action) {
+      state.keepAliveList = action.payload
     }
   }
 })
 
-export const { setCollapsed, setTheme, setMenuMode, setWidth } = appSlice.actions
+export const { setCollapsed, setTheme, setMenuMode, setWidth, setKeepAliveList} = appSlice.actions
 
 export const selectTheme = (state: RootState) => state.app.theme
 export const selectCollapsed = (state: RootState) => state.app.collapsed
 export const selectMenuMode = (state: RootState) => state.app.menuMode
+export const selectKeepAliveList = (state: RootState) => state.app.keepAliveList
 
 export default appSlice.reducer
