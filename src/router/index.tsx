0f7dc14e138b05b583d3e79103abe9e089cd978c
wangyi <2851813754@qq.com>
2022-05-16 15:34:27 +0800

    config🔧:  路由入口文件调整

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,28 +1,23 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-import { ConfigProvider } from 'antd'
-import {routes} from "@/router/configure";
+import { routes } from "@/router/configure";
 
-import zhCN from 'antd/es/locale/zh_CN'
+
 import { map } from 'ramda'
 import { SuspenseLoading } from '@/components/Loading'
 
 // 创建 同步路由文件
 export const SyncRouter = (): JSX.Element => {
-	return (
-		<ConfigProvider locale={zhCN}>
-			<BrowserRouter>
-				<SuspenseLoading>
-					<Routes>
-						{map(
-							(route) => (
-								<Route path={route.path} key={route.name} element={<route.component route={route} />} />
-							),
-							routes
-						)}
-					</Routes>
-				</SuspenseLoading>
-			</BrowserRouter>
-		</ConfigProvider>
-	)
+  return (
+    <SuspenseLoading>
+      <Routes>
+        {map(
+          (route) => (
+            <Route path={route.path} key={route.name} element={<route.component route={route} />} />
+          ),
+          routes
+        )}
+      </Routes>
+    </SuspenseLoading>
+  )
 }
