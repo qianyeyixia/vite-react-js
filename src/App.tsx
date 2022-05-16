12d507ede41dda149ccb78458f30d088db844182
wangyi <2851813754@qq.com>
2022-05-16 15:32:00 +0800

    config🔧:  入口文件更新

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
 import React, { StrictMode, FC } from "react";
-
+import { BrowserRouter } from "react-router-dom"
 import { Provider as ReduxProvider } from "react-redux";
 import { PersistGate } from "redux-persist/lib/integration/react";
-import BackLayout from "@/layout";
+import { ConfigProvider } from 'antd'
 
-
+import zhCN from 'antd/es/locale/zh_CN'
 import { store, persistor } from "@/store";
 import '@/assets/css/public.less';
 
 import { SyncRouter } from '@/router'
 
-const App:FC = () => {
+const App: FC = () => {
   return (
     <StrictMode>
       <ReduxProvider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <SyncRouter/>
+          <ConfigProvider locale={zhCN}>
+            <BrowserRouter>
+              <SyncRouter />
+            </BrowserRouter>
+          </ConfigProvider>
         </PersistGate>
-        </ReduxProvider>
+      </ReduxProvider>
     </StrictMode>
   );
 };
